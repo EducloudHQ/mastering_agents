11e6acae5e21b159f655d2ae976a5983e1b25168
trey-rosius <treyrosius@gmail.com>
2025-03-27 09:58:32 +0100

    more updates

--- agents_intro/agents_intro_stack.py
+++ agents_intro/agents_intro_stack.py
@@ -11,9 +11,13 @@ from aws_cdk.aws_secretsmanager import Secret
 from constructs import Construct
 from cdklabs.generative_ai_cdk_constructs.bedrock import (
     Agent,
+    AgentCollaborator,
+    AgentCollaboratorType,
+    RelayConversationHistoryType,
     BedrockFoundationModel,
     AgentActionGroup,
     ActionGroupExecutor,
+    AgentAlias,
     KnowledgeBaseType,
     KnowledgeBaseBase,
 
@@ -53,14 +57,13 @@ class AgentsIntroStack(Stack):
         todo_table.grant_full_access(action_group_function)
         action_group_function.add_environment("TODO_TABLE", todo_table.table_name)
 
-        agent = Agent(
+        todo_agent = Agent(
             self,
             "Agent",
 
             foundation_model=BedrockFoundationModel.ANTHROPIC_CLAUDE_3_5_SONNET_V1_0,
-            instruction="You are a helpful and friendly agent that perfroms multiple actions on a dynamodb table",
+            instruction="You are a helpful and friendly agent that performs CRUDL operations on a dynamodb table",
         )
-
         action_group: AgentActionGroup = AgentActionGroup(
             name="TodoApiSupport",
             description="Use these functions to create/update/delete and list todo items",
@@ -71,20 +74,96 @@ class AgentsIntroStack(Stack):
             api_schema=ApiSchema.from_local_asset("./lambda/openapi.json"),
         )
 
-        agent.add_alias(
-            alias_name="todo_agent_alias", description="Alias for description"
+
+        todo_agent_alias = AgentAlias(
+            self,
+            "AgentAlias",
+            agent=todo_agent,
+            alias_name="todo_agent_alias",
+            description="Todo Alias for description"
+        )
+        customer_support_agent = Agent(
+            self,
+            "CustomerSupportAgent",
+
+            foundation_model=BedrockFoundationModel.ANTHROPIC_CLAUDE_3_5_SONNET_V1_0,
+            instruction="You Specialize in answering customer support questions for our workshops",
         )
 
-        agent.add_action_group(action_group)
-        pineconevs = PineconeVectorStore(
-            connection_string='https://ai-learning-app-kb-pbfqwcb.svc.aped-4627-b74a.pinecone.io',
-            credentials_secret_arn='arn:aws:secretsmanager:us-east-1:132260253285:secret:bedrock-pinecone-apikey-aToyuv',
+        support_agent_alias = AgentAlias(
+            self,
+            "CustomerSupportAgentAlias",
+            agent=customer_support_agent,
+            alias_name="customer_support_agent_alias",
+            description="Customer Support Agent Alias for description"
+        )
+
+        content_generation_agent = Agent(
+            self,
+            "ContentGenerationAgent",
+
+            foundation_model=BedrockFoundationModel.ANTHROPIC_CLAUDE_3_5_SONNET_V1_0,
+            instruction="You Specialize in answering content generation questions for our workshops",
+        )
+
+        content_generation_agent_alias = AgentAlias(
+            self,
+            "ContentGenerationAgentAlias",
+            agent=content_generation_agent,
+            alias_name="content_generation_agent_alias",
+            description="Content Generation Agent Alias for description"
+        )
+
+        supervisor_agent = Agent(
+            self,
+            "SuperVisorAgent",
+            foundation_model=BedrockFoundationModel.ANTHROPIC_CLAUDE_3_5_SONNET_V1_0,
+            instruction=(
+                "You are a helpful and friendly supervisor agent that routes user queries to the appropriate specialized agents. "
+                "Analyze the user's request carefully and determine the most suitable agent based on the following roles: "
+                "(1) Route questions regarding todo item management (create, read, update, delete, list tasks) to the TodoAgent. "
+                "(2) Route customer support related questions, such as troubleshooting or workshop inquiries, to the CustomerSupportAgent. "
+                "(3) Route content generation requests like creating summaries, workshop flashcards, or general content-related questions to the ContentGenerationAgent. "
+                "If the user's query doesn't fit into any specialized category, you may provide general assistance directly."
+            ),
+            agent_collaboration=AgentCollaboratorType.SUPERVISOR,
+            agent_collaborators=[
+                AgentCollaborator(
+                    agent_alias=todo_agent_alias,
+                    collaborator_name="TodoAgent",
+                    collaboration_instruction="Route Todo questions to this agent",
+                    relay_conversation_history=True
+
+                ),
+                AgentCollaborator(
+                    agent_alias=support_agent_alias,
+                    collaborator_name="CustomerSupportAgent",
+                    collaboration_instruction="Route Customer support(FAQ) questions to this agent",
+                    relay_conversation_history=True
+
+                ),
+                AgentCollaborator(
+                    agent_alias=content_generation_agent_alias,
+                    collaborator_name="WorkshopContentGenerationAgent",
+                    collaboration_instruction="Route all workshop content generation questions to this agent",
+                    relay_conversation_history=True
+
+                ),
+            ]
+
+        )
+
+
+        todo_agent.add_action_group(action_group)
+        pinecone_vec = PineconeVectorStore(
+            connection_string='https://ai-fddfghggklkjnlknjmvjhvbhjbvjhvbjvhfghbnvbjhv.io',
+            credentials_secret_arn='arn:aws:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxaToyuv',
             text_field='text',
             metadata_field='metadata'
         )
 
         kb = VectorKnowledgeBase(self, 'KnowledgeBaseAgent',
-                                 vector_store=pineconevs,
+                                 vector_store=pinecone_vec,
                                  embeddings_model=BedrockFoundationModel.TITAN_EMBED_TEXT_V2_1024,
                                  instruction='Use this knowledge base to summarize,generate QA and flash cards about workshops ' +
                                              'It contains some workshops gotten from educloud academy.'
@@ -105,6 +184,6 @@ class AgentsIntroStack(Stack):
                      chunking_strategy=ChunkingStrategy.FIXED_SIZE,
                      )
 
-        agent.add_knowledge_base(knowledge_base=kb)
+        content_generation_agent.add_knowledge_base(knowledge_base=kb)
         kb.grant_retrieve_and_generate(action_group_function)
         action_group_function.add_environment("KNOWLEDGE_BASE_ID",kb.knowledge_base_id)
